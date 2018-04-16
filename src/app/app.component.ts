c89bae60612f1efc88b3da2d6cb241aa9319f424
itsvinayonline <itsvinayonline@gmail.comm>
2018-04-16 21:19:20 +0530

    reverting the changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,21 @@ import {
   CalendarEventTimesChangedEvent
 } from 'angular-calendar';
 
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
+
 @Component({
   selector: 'app-demo-component',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -46,6 +61,7 @@ export class DemoComponent {
     @ViewChild('modalContent') modalContent: TemplateRef<any>;
     view = 'month';
     viewDate: Date = new Date();
+    activeDayIsOpen = true;
 
     refresh: Subject<any> = new Subject();
 
@@ -54,11 +70,19 @@ export class DemoComponent {
       start: subDays(startOfDay(new Date()), 1),
       end: addDays(new Date(), 1),
       title: 'A 3 day event',
+      color: colors.red,
     },
     {
       start: addHours(startOfDay(new Date()), 2),
       end: new Date(),
       title: 'A draggable and resizable event',
+      color: colors.yellow,
+
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
     }
   ];
 
@@ -68,6 +92,22 @@ export class DemoComponent {
   };
 
   public DemoComponent() {
+  }
+
+
+  // constructor(private modal: NgbModal) {}
+   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
   }
 
   eventTimesChanged({
@@ -91,6 +131,7 @@ export class DemoComponent {
       title: 'New event',
       start: startOfDay(new Date()),
       end: endOfDay(new Date()),
+      color: colors.red,
       draggable: true,
       resizable: {
         beforeStart: true,
@@ -115,7 +156,7 @@ export class DemoComponent {
   //   });
   //   this.refresh.next();
   }
-  cancelPopu() {
+  cancelPopu(){
     this.addAppointmentPopu = true;
   }
   submiApportment(data) {
@@ -124,6 +165,7 @@ export class DemoComponent {
     title: data.meetingTitle,
     start: startOfDay(new Date()),
     end: endOfDay(new Date()),
+    color: colors.red,
     draggable: false,
     resizable: {
       beforeStart: false,
