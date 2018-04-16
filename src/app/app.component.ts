286d5ab534442c97bbf04f3349a6b23b70be22cd
itsvinayonline <itsvinayonline@gmail.comm>
2018-04-16 21:15:10 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,21 +23,6 @@ import {
   CalendarEventTimesChangedEvent
 } from 'angular-calendar';
 
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3'
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF'
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA'
-  }
-};
-
 @Component({
   selector: 'app-demo-component',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -61,22 +46,6 @@ export class DemoComponent {
     @ViewChild('modalContent') modalContent: TemplateRef<any>;
     view = 'month';
     viewDate: Date = new Date();
-    activeDayIsOpen = true;
-    actions: CalendarEventAction[] = [
-      {
-        label: '111<i class="fa fa-fw fa-pencil"></i>',
-        onClick: ({ event }: { event: CalendarEvent }): void => {
-          this.handleEvent('Edited', event);
-        }
-      },
-      {
-        label: '<i class="fa fa-fw fa-times"></i>',
-        onClick: ({ event }: { event: CalendarEvent }): void => {
-          this.events = this.events.filter(iEvent => iEvent !== event);
-          this.handleEvent('Deleted', event);
-        }
-      }
-    ];
 
     refresh: Subject<any> = new Subject();
 
@@ -85,20 +54,11 @@ export class DemoComponent {
       start: subDays(startOfDay(new Date()), 1),
       end: addDays(new Date(), 1),
       title: 'A 3 day event',
-      color: colors.red,
-      actions: this.actions
     },
     {
       start: addHours(startOfDay(new Date()), 2),
       end: new Date(),
       title: 'A draggable and resizable event',
-      color: colors.yellow,
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
     }
   ];
 
@@ -108,22 +68,6 @@ export class DemoComponent {
   };
 
   public DemoComponent() {
-  }
-
-
-  // constructor(private modal: NgbModal) {}
-   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
-    if (isSameMonth(date, this.viewDate)) {
-      if (
-        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
-        events.length === 0
-      ) {
-        this.activeDayIsOpen = false;
-      } else {
-        this.activeDayIsOpen = true;
-        this.viewDate = date;
-      }
-    }
   }
 
   eventTimesChanged({
@@ -147,7 +91,6 @@ export class DemoComponent {
       title: 'New event',
       start: startOfDay(new Date()),
       end: endOfDay(new Date()),
-      color: colors.red,
       draggable: true,
       resizable: {
         beforeStart: true,
@@ -181,7 +124,6 @@ export class DemoComponent {
     title: data.meetingTitle,
     start: startOfDay(new Date()),
     end: endOfDay(new Date()),
-    color: colors.red,
     draggable: false,
     resizable: {
       beforeStart: false,
